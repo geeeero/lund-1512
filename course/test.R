cd503129e5ae63209d80f842a202a02d3631e6ef
Gero Walter <gero@gmx.at>
2015-12-06 12:46:54 +0100

    course folder

---
+++ course/test.R
@@ -0,0 +1,10 @@
+
+#install.packages("luck", repos="http://R-Forge.R-project.org")
+
+# download .tar.gz file
+install.packages("luck_0.9.tar.gz", repos = NULL, type = "source")
+# directly
+install.packages("http://download.r-forge.r-project.org/src/contrib/luck_0.9.tar.gz", repos = NULL, type = "source")
+
+
+#
