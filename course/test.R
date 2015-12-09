7547b7a28563231d9b78964db38cc853b0c2beed
Gero Walter <g.m.walter@tue.nl>
2015-12-09 20:28:28 +0100

    set shape plots

--- course/test.R
+++ course/test.R
@@ -6,5 +6,10 @@ install.packages("luck_0.9.tar.gz", repos = NULL, type = "source")
 # directly
 install.packages("http://download.r-forge.r-project.org/src/contrib/luck_0.9.tar.gz", repos = NULL, type = "source")
 
+library(luck)
+
+?LuckModel
+?luck::plot
+
 
 #
